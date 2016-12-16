bd9d6c35d0c843b534a2a18716dbb5fb63ff2e7a
danjia21 <danjia1992@gmail.com>
2016-12-16 17:48:31 +0100

    Implemented trangulation with 8pts RANSAC.

--- src/triangulation/triangulationRansac.m
+++ src/triangulation/triangulationRansac.m
@@ -1,7 +1,14 @@
 function [P, inlier_mask] = triangulationRansac(kp1, kp2, M1, M2, Param)
 
-   % 1 point RANSAC to filter outlier
-    inlier_mask = ransac1Point(kp1, kp2, Param); 
+%     % 1 point RANSAC to filter outlier
+%     inlier_mask = ransac1Point(kp1, kp2, Param); 
+    
+    % 8 point RANSAC to filter outlier
+    p1 = Param.K \ [kp1, ones(size(kp1, 1), 1)].';
+    p2 = Param.K \ [kp2, ones(size(kp2, 1), 1)].';
+    [~, inlier_idx] = ransacfitfundmatrix(p1, p2, Param.ransac8pt_threshold);
+    inlier_mask = 1:size(kp1, 1); 
+    inlier_mask = ismember(inlier_mask, inlier_idx);
     
     % triangulation
     p1 = kp1(inlier_mask, :); p1 = [p1, ones(size(p1, 1), 1)].';
